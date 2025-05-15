22bd166f88d97801130ffda22828589a5df9730f
Maxim <maks.degtearyov25@gmail.com>
2025-05-15 21:55:40 +0300

    Реализовал TagService

--- back/metadata-service/Application/Services/ITagService.cs
+++ back/metadata-service/Application/Services/ITagService.cs
@@ -1,6 +1,18 @@
+using MetadataService.DTOs;
+
 namespace MetadataService.Application.Services;
 
 public interface ITagService
 {
+    Task<TagDto> GetAsync(int id, CancellationToken ct = default);
     
+    Task<IReadOnlyList<TagDto>> ListAsync(CancellationToken ct = default);
+    
+    Task<int> CreateAsync(CreateTagRequest request, CancellationToken ct = default);
+    
+    Task ApproveAsync(int id, CancellationToken ct = default);
+    
+    Task UpdateAsync(int id, UpdateTagRequest request, CancellationToken ct = default);
+    
+    Task DeleteAsync(int id, CancellationToken ct = default);
 }
