31f016fec0aabb287ce627bec51a64de6f462c99
Maxim <maks.degtearyov25@gmail.com>
2025-05-16 12:53:37 +0300

    реализовал songbuilder

---
+++ back/metadata-service/Domain/Builders/ISongBuilder.cs
@@ -0,0 +1,16 @@
+using MetadataService.Models;
+
+namespace MetadataService.Domain.Builders;
+
+public interface ISongBuilder
+{
+    ISongBuilder For(Song song);
+    
+    Task<ISongBuilder> AttachAuthorsAsync(Guid[] authorIds, CancellationToken ct = default);
+    
+    Task<ISongBuilder> AttachTagsAsync(int[] tagIds, CancellationToken ct = default);
+    
+    ISongBuilder AddTranscriptSegment(Transcript segment);
+    
+    Task<Song> BuildAsync(CancellationToken ct = default);
+}
